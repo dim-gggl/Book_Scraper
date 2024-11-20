eb3ebb1b271687d30a875e380af56f9569a581e7
dim-gggl <dimitri.gaggioli@gmail.com>
2024-11-20 01:57:04 +0100

    Révision du script_phase_1 terminée

--- scripts/script_phase_1.py
+++ scripts/script_phase_1.py
@@ -1,80 +1,70 @@
-# Import des packages et modules nécessaires à l'extraction de données
 import requests
 from bs4 import BeautifulSoup
-
-# Import de csv pour le transfert des données sur un fichier
 import csv
 
+
 root = "https://books.toscrape.com/"
-data_category = [
+product_data_headers = [
     "product_url", "universal_product_code", "title", "price_including_tax",
     "price_excluding_tax", "number_available", "product_description",
     "category", "review_rating", "image_url"
 ]
+output_file_rep = f'../data'
 
-def scrape_book(book_url):
-    # Récupération du code source de la page via une requête HTTP
-    response = requests.get(book_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Test pour vérifier qu'on a bien obtenu le code
-    # print(soup) ––––> OK
+def scrape_book(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Après inspection sur le site de chaque élément à extraire,
-    # on peut les isoler dans des variables en utilisant leurs
-    # balises, id et/ou class via BeautifulSoup :
-    product_url = book_url
-    upc = soup.find('th', string="UPC").find_next('td').text
-    title = soup.find('h1').text
-    price_incl_tax_raw = soup.find('th', string="Price (incl. tax)").find_next('td').text
-    price_incl_tax = str(price_incl_tax_raw.lstrip('Â£') + " £")
-    price_excl_tax_raw = soup.find('th', string="Price (excl. tax)").find_next('td').text
-    price_excl_tax = str(price_excl_tax_raw.lstrip('Â£') + " £")
-    number_available = soup.find('th', string="Availability").find_next('td').text
-    product_description = soup.find('div', id="product_description").find_next('p').text
-    category = soup.find('ul', class_='breadcrumb').find_all('a')[-1].text
+        product_url = str(url)
+        upc = soup.find('th', string="UPC").find_next('td').text
+        title = soup.find('h1').text
 
-    # Le paragraphe contenant le review_rating affiche dans sa balise:
-    # <p class='star-rating Four'> Il faut distinguer 2 classes ici indexées
-    # 0 pour 'star-rating' et 1 pour 'Four'
-    rating_tag = soup.find('p', class_='star-rating')
-    rating_class = rating_tag['class'][1]
-    # Dictionnaire pour convertir le texte en valeur numérique
-    ratings_map = {
-        "One": "1/5",
-        "Two": "2/5",
-        "Three": "3/5",
-        "Four": "4/5",
-        "Five": "5/5"
-    }
-    # Conversion de la note en utilisant le dictionnaire
-    review_rating = ratings_map.get(rating_class, "Note non trouvée")
+        price_incl_tax_raw = soup.find('th', string="Price (incl. tax)").find_next('td').text.strip()
+        price_incl_tax = f"£ {price_incl_tax_raw.lstrip('Â£')}"
+        price_excl_tax_raw = soup.find('th', string="Price (excl. tax)").find_next('td').text
+        price_excl_tax = f"£ {price_excl_tax_raw.lstrip('Â£')}"
 
-    image_url = soup.find('img')['src']
-    image_url = 'https://books.toscrape.com/' + image_url.lstrip('../..')
+        number_available = soup.find('th', string="Availability").find_next('td').text
+        product_description = soup.find('div', id="product_description").find_next('p').text
+        category = soup.find('ul', class_='breadcrumb').find_all('a')[-1].text
 
-    # En vue de l'écriture des données extraites dans un fichier .csv
-    # organisé, on commence par distinguer, dans 2 listes, les noms
-    # des catégories de donnée et les variables qui contiennent leurs valeurs :
+        rating_tag = soup.find('p', class_='star-rating')
+        rating_class = rating_tag['class'][1]
 
-    data_values = [
-        product_url, upc, title, price_incl_tax, price_excl_tax, number_available,
-        product_description, category, review_rating, image_url
-    ]
+        # Dictionnaire pour convertir le texte en valeur numérique
 
-    print(data_values)
+        ratings_map = {
+            "One": "1/5",
+            "Two": "2/5",
+            "Three": "3/5",
+            "Four": "4/5",
+            "Five": "5/5"
+        }
 
-    # Écriture du fichier .csv dans le répertoire data de notre projet
-    # en utilisant csv importé en début de script.
-    # On ouvre ce nouveau fichier avec le mode writer et en spécifiant
-    # à Python d'ignorer les caractères de fin de ligne et d'éviter l'ajout
-    # de lignes vides intermédiaires.
-    return data_values
+        review_rating = ratings_map.get(rating_class, "Note non trouvée")
+
+        image_url_src = soup.find('img')['src']
+        image_url = root + image_url_src.lstrip('../..')
+
+        return [
+            product_url, upc, title, price_incl_tax, price_excl_tax, number_available,
+            product_description, category, review_rating, image_url
+        ]
+    else:
+        return None
 
 if __name__ == '__main__':
-    url = f'{ root }catalogue/the-boys-in-the-boat-nine-americans-and-their-epic-quest-for-gold-at-the-1936-berlin-olympics_992/index.html' #noqa
-    data_values = scrape_book(url)
-    with open('../data/product_datas.csv', mode='w', newline='', encoding="utf-8") as file :
-        writer = csv.writer(file, delimiter=',') # On spécifie la ',' pour séparer les données.
-        writer.writerow(data_category) # La ligne d'en-tête, contenant les catégories
-        writer.writerow(data_values) # La ligne de données
+    url = f'{root}catalogue/the-boys-in-the-boat-nine-americans-and-their-epic-quest-for-gold-at-the-1936-berlin-olympics_992/index.html' #noqa
+    product_data = scrape_book(url)
+    if product_data:
+        try:
+            with open(f'{output_file_rep}/single_book_datas.csv', mode='w', newline='', encoding="utf-8") as file :
+                writer = csv.writer(file)
+                writer.writerow(product_data_headers) # La ligne d'en-tête, contenant les catégories
+                writer.writerow(product_data) # La ligne de données
+        except FileNotFoundError:
+            print(f"Erreur : {output_file_rep} est invalide")
+    else:
+        print("Erreur : impossible de récupérer les données du livres")
--- scripts/script_phase_2.py
+++ scripts/script_phase_2.py
@@ -1,55 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from script_phase_1 import scrape_book, data_category
+from script_phase_1 import (scrape_book, root,
+                            product_data_headers, output_file_rep, product_data)
 
-# On commence par la création d'une fonction pour lister les liens
-# de tous les livres d'une page
+
 def get_product_links(soup):
-    # On crée la liste qui contiendra liens extraits
     product_links = []
     for h3 in soup.find_all('h3'):
         raw_link = h3.a['href']
-        product_link = "https://books.toscrape.com/catalogue/" + raw_link.lstrip('../../')
+        product_link = f"{root}catalogue/{raw_link.lstrip('../../')}"
         product_links.append(product_link)
+    print(product_links)
     return product_links
 
-def scrape_category(category_url):
-    current_url = category_url # Cette variable est celle qui va éplucher les pages,
-    # on spécifie ici que son point de départ est la page d'accueil de la catégorie
-    product_links = [] # Notre future liste de liens
 
-    # current_url s'arrêtera à la page qui ne contient plus de page suivante,
+
+def scrape_category(category_url):
+
+    current_url = category_url
+    product_links = []
     # le nombre de pages différant d'une catégorie à l'autre, on préfère une boucle
     # while
     while current_url:
         response = requests.get(current_url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        links = get_product_links(soup) # Tant que la variable contiendra un lien,
-        print(links)
-        # les liens de chaque livre qui s'y trouvent seront extraits
-        product_links.extend(links) # et ajoutés à cette liste
+        links = get_product_links(soup)
+        if links:
+            print(f"{len(links)} de liens trouvés sur {current_url}")
+            product_links.extend(links)
+            print(product_links)
+
         next_page_tag = soup.find('li', class_='next') # On y cherche l'accès page suivante
         if next_page_tag:
-            next_page_url = next_page_tag.find('a')['href'] # On extrait le morceau de lien
-            # Et on l'ajoute minutieusement à l'URL du site :
+            next_page_url = next_page_tag.find('a')['href']
             current_url = '/'.join(current_url.split('/')[:-1]) + '/' + next_page_url
         else:
-            current_url = None # Lorsque le scraping de la dernière page est fait, la variable
-            # ne contient plus de lien vers une autre page. La boucle s'arrête.
+            current_url = None
+            print("No tag found for next page url")
 
+    if not product_links:
+        print("No product links found")
+        return
+
+    # On prépare le fichiers CSV
     category_name = category_url.rstrip('./').split('/')[-1]
-    with open(f'../data/{category_name}_product_details.csv', mode='w', newline='', encoding="utf-8") as file:
-        data_values = []
-        writer = csv.writer(file, delimiter=',')
+    output_file = f'{output_file_rep}{category_name}_product_details.csv'
+
+
+    with open(output_file, mode='w', newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
         writer.writerow(data_category)
 
-        for link in product_links:
-            data_values += scrape_book(link)
-
-        writer.writerows(data_values)
+    for link in product_links:
+        product_data += scrape_book(link)
+        writer.writerows(product_data)
 
 
 if __name__ == '__main__':
-    category_url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/'
+    category_url = f"'{root}catalogue/category/books/mystery_3/"
     scrape_category(category_url)
